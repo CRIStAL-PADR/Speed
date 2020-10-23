49dd76cc2ba298c0535ace4de6db6345356e1d31
Fabrice dedo <dsamfabrice5@gmail.com>
2020-10-23 16:17:58 +0200

    Correction on the multi-crane API

--- speedlib/cranes/faller.py
+++ speedlib/cranes/faller.py
@@ -4,10 +4,10 @@
 import requests
 import re
 import time
-crane_number = []
 
-IP_master_n = []
-IP_slave_n = []
+
+ip_master_n = []
+ip_slave_n = []
 
 MotorSpreader = 1
 MotorCrab = 2
@@ -18,29 +18,39 @@ MotorDirectionBackward = -1
 
 # The amount of time to use before the HTTP connection to the board is considered as too long and we thus exit
 # by throwing an RuntimeError of type requests.RuntimeErrors.ConnectTimoutRuntimeError. 
+
 timeout = 2.5
 
 
-def getMotorInfoFromNumber(sNr, n = 0):
+def get_motor_info_from_number(sNr, n = 0):
     """ Comments about getMotorInfoFromNumber function
-    It takes the number of the motor as input and returns the  number with its IP address.
+    It takes the number of the motor and the crane as input and returns the  number of the motor with its IP address.
     :param : see def start (sNr, turn) parameters
     
     """
     
+    if n < 0:
+        raise ValueError()
     
-    IP_master = IP_master_n[n] 
-    IP_slave = IP_slave_n[n] 
-    
+    if not isinstance(n, int):
+        raise TypeError()
+        
     if not isinstance(sNr, int):
         raise TypeError()
     
+    ip_master = ip_master_n[n] 
+    ip_slave = ip_slave_n[n] 
+    
+    
+    
+    
+    
     if MotorSpreader == sNr:
-        return (IP_slave, 1)
+        return (ip_slave, 1)
     elif MotorCrab == sNr:
-        return (IP_slave, 2)
+        return (ip_slave, 2)
     elif MotorChassis == sNr:
-        return (IP_master, 1)
+        return (ip_master, 1)
         
     raise RuntimeError("Invalid motor number, must be MotorSpreader, MotorCrab or MotorChassis, got "+str(sNr))
     
@@ -53,7 +63,7 @@ def step(sNr,turn,n=0):
     Example:
         start(MotorSpreader, MotorDirectionBackward)  turns the spreader backwards
     """
-    start(sNr, turn, n = 0)
+    start(sNr, turn, n)
 
 
     
@@ -64,13 +74,6 @@ def start(sNr, turn, n = 0):
         :param sNr: This is the first of the arguments. It indicates the engine number that we would like to start. It takes the values ​​1 2 3 which indicates motors 1 2 and 3 respectively
         :param turn: It indicates the direction in which we would like to run the engine. 1 for moving forward and -1 for moving backward.
 
-    Example:
-        craneGauche = fallerlib.init("172.168.1.0") ## craneA = 0 
-        craneDroite = fallerlib.init("172.167.2.0") ## craneB = 1
-        
-        fallerlib.start(craneGauche, MotorChassis, Forward) 
-        fallerlib.start(craneDroit, MotorChassis, Forward)
-        fallerlib.start(craneGauche, MotorCrub, Forward) 
 
     """
     global timeout
@@ -78,7 +81,7 @@ def start(sNr, turn, n = 0):
     if turn not in [MotorDirectionForward, MotorDirectionBackward]:
         raise RuntimeError("Invalid parameter, turn must be either MotorDirectionForward or MotorDirectionBackward. Got " +str(turn))			
     
-    ip, numMotor = getMotorInfoFromNumber( sNr, n=0)
+    ip, numMotor = get_motor_info_from_number( sNr, n)
     r = requests.get("http://"+ip+"/startM?sNr="+str(numMotor)+"&turn="+str(turn),timeout=timeout)
  
     if r.status_code !=200:
@@ -89,31 +92,45 @@ def start(sNr, turn, n = 0):
         
         
 def set_speed(sNr, speed, n = 0):
-    """ set the speed for a motor """    		
+    """ set the speed for a motor """    	
+    
+    if not isinstance(n, int):
+        raise TypeError()
+    if n < 0:
+        raise ValueError()	
+    if n > len(ip_master_n):
+        raise IndexError()
+    
+    
     current = change_speed(n,sNr, 0) 
     diff = speed - current 
     return change_speed(sNr, diff , n=0)    
 
 def get_speed(sNr, n=0):
     """ Returns the current speed for a motor """    		
-    return change_speed(sNr, 0 , n = 0)
+    return change_speed(sNr, diff , n = 0)
 
 
 
 def change_speed(sNr, diff, n=0):
-    global IP_master, IP_slave, timeout
-    """ Comments about changePWM function
+    global ip_master, ip_slave, timeout
+    """ Comments about change_speed function
 	This function allows us to modify the engine speed while varying its cyclic ratio. 
 	
-       :param sNr:  see def start (sNr, turn) parameters
-       :param diff: This parameter is used to vary the speed of the motor. It takes the values between 0 and 100 for the acceleration and 0 and -70 for the slowdown.
-       It should be noted that the maximum speed that the motor can reach is 100.
-	   
+       :param sNr:  see def start (sNr, turn, n = 0) parameters
+       :param diff: This parameter is used to vary the speed of the motor. Il s'agit d'un entier.
+       It should be noted that the maximum speed that the motor can reach is 100 and the motor speed cannot drop below 30
+	   :n : Il is the number of the crane
      Example:
-		changePWMM(3, -60) : allows to decrease the motor speed 3 by 60
+		change_speed( MotorSpreader, -60, 2 ) : allows to decrease the motorspeed 3 by 60
 		
      """
-    ip, numMotor = getMotorInfoFromNumber(sNr, n = 0)
+		
+    
+    if not isinstance (diff, int):
+        raise TypeError()
+        
+    ip, numMotor = get_motor_info_from_number(sNr, n)
     r = requests.get("http://"+ip+"/changePWMTV?sNr="+str(numMotor)+"&diff="+str(diff), timeout=timeout)
     
     if r.status_code != 200: 
@@ -128,13 +145,18 @@ def change_speed(sNr, diff, n=0):
 
 
 def get_battery(n=0):
-    global IP_master_n, IP_slave_n, timeout
+    """Comment about get_battery function
+   
+    This function takes the number of the crane as a parameter and returns a tuple 
+    (the battery state associated with the crane: the battery of the master as well as that of the slave)
+    """
+    global ip_master_n, ip_slave_n, timeout
 
-    IP_master = IP_master_n[n] 
-    IP_slave = IP_slave_n[n] 
+    ip_master = ip_master_n[n] 
+    ip_slave = ip_slave_n[n] 
 
-    r1=requests.get("http://"+IP_master+"/getBat?n=1", timeout=timeout)
-    r2=requests.get("http://"+IP_slave+"/getBat?n=2", timeout=timeout)
+    r1=requests.get("http://"+ip_master+"/getBat?n=1", timeout=timeout)
+    r2=requests.get("http://"+ip_slave+"/getBat?n=2", timeout=timeout)
 
     if r1.status_code != 200: 
         raise RuntimeError("Please check if the r1 battery is correctly powered")
@@ -149,29 +171,21 @@ def init(ip):
     """ 
     The purpose of this function is to initialize the IP address of the master and once the IP address of the master is
     initialized to obtain that of the slave thanks to the getOtherEsp function   
-    
-    Example:
-        craneA = fallerlib.init("172.168.1.0") # len(IP_masster_n)-1 = 0 
-        craneD = fallerlib.init("172.167.2.0")
-        craneE = fallerlib.init("215.1.2.0")
-    
-        fallerlib.start(craneA, MotorChassis, Forward) 
-        fallerlib.start(craneB, MotorChassis, Forward) 
     """
 
-    global IP_master_n, IP_slave_n
-    IP_master_n.append(ip) 
-    IP_slave_n.append( get_other_esp(ip) )
-    crane_number.append(len(IP_master_n)-1)
+    global ip_master_n, ip_slave_n
+    ip_master_n.append(ip) 
+    ip_slave_n.append( get_other_esp(ip) )
+    
  
-    return len(IP_master_n)-1   
+    return len(ip_master_n)-1   
     
     
     
 
     
 def get_other_esp(ip):
-    """ Comments about changePWM function
+    """ Comments about get_other_esp function
 	This function has the role of launching a request to obtain the IP address of the slave
     """
     global timeout
@@ -186,13 +200,15 @@ if __name__ == "__main__":
     ip = "172.17.217.217"
     
     init(ip)
-    print(crane_number[0])
+    
+    
+    print(init(ip))
     
     init(ip)
-    print(crane_number[1])
-    #step(MotorSpreader, MotorDirectionBackward,2)
+    
+    set_speed(MotorCrab, 1, 5)
+    
 
-    print(crane_number)
     
   
     
