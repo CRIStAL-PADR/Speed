16d3025f548a4fff9cd8d017ff7550eca863157d
Fabrice dedo <dsamfabrice5@gmail.com>
2020-10-23 16:24:27 +0200

    Added test to manage multi-crane API

--- tests/test_crane.py
+++ tests/test_crane.py
@@ -1,3 +1,6 @@
+# copyright CNRS
+#
+#
 import unittest
 import requests
 from speedlib.cranes import faller
@@ -12,17 +15,18 @@ class TestCrane(unittest.TestCase):
             faller.init("172.168.1.1")
 
     def test_init_with_ip(self):
-        """Test that the init function is returning an exception if an invalid IP is given"""       
+        """ Test that the init function works when a correct ip given"""       
         faller.init("172.17.217.217")
     
     def test_change_speed(self):
+        """ test that the change_speed function works when given the right ones settings"""  
         faller.init("172.17.217.217")
         for motor in [faller.MotorCrab, faller.MotorChassis, faller.MotorSpreader]:
-            t = faller.set_speed(motor, 50)              
-            self.assertEqual(t, 50)             
+            t = faller.set_speed(motor, 50,n)              
+            self.assertEqual(t, 50, n )             
         
     def test_change_speed(self):
-        """Test that the init function is returning an exception if an invalid IP is given"""       
+        """ test that the change_speed function works when given the right ones settings"""         
         faller.init("172.17.217.217")
         for motor in [faller.MotorCrab, faller.MotorChassis, faller.MotorSpreader]:
             t = faller.change_speed(motor, 0)  
@@ -34,27 +38,28 @@ class TestCrane(unittest.TestCase):
                 self.assertNotEqual(t,t2)
 
     def test_change_speed_invalid(self):
+        """Test that the change_speed function is returning an exception if an invalid parameters is given""" 
         faller.init("172.17.217.217")
         # Here we test that a string is not considered as a valid parameter
         with self.assertRaises(TypeError):
             t = faller.change_speed("MotorOne", 10)        
 
-        # Here we test that an out of range motor number rise an exception
+        
         with self.assertRaises(RuntimeError):
             t = faller.change_speed(123, 20)        
 
-        ## Here we tests that weird values provided as parameter are rejected with an exception
+        # Here we tests that weird values provided as parameter are rejected with an exception
         with self.assertRaises(TypeError):
             for motor in [faller.MotorCrab, faller.MotorChassis, faller.MotorSpreader]:
-                t = faller.change_speed(motor, "20")        
+                t = faller.change_speed(motor, "20")       
 
     def test_step(self):
         faller.init("172.17.217.217")
         for motor in [faller.MotorCrab, faller.MotorChassis, faller.MotorSpreader]:
             for direction in [faller.MotorDirectionForward, faller.MotorDirectionBackward]:        
                 faller.step(motor, direction)        
-
-
+    
+        # Here we test that an out of range motor number rise an exception
     def test_start(self):
         faller.init("172.17.217.217")
         for motor in [faller.MotorCrab, faller.MotorChassis, faller.MotorSpreader]:
@@ -63,30 +68,23 @@ class TestCrane(unittest.TestCase):
 
     def test_startInvalid(self):
         faller.init("172.17.217.217")
+        
+        # Here we test that an out of range motor number rise an exception
         with self.assertRaises(RuntimeError):
-            faller.start(-5, faller.MotorDirectionForward)        
-
+            faller.start(-5, faller.MotorDirectionForward)  
+                  
+        # Here we test that an out of turn rise an exception
         with self.assertRaises(RuntimeError):
             faller.start(faller.MotorCrab, -10)        
 
+        # Here we tests that weird values provided as parameter are rejected with an exception
         with self.assertRaises(Exception):
             faller.start("Yo", -10) 
-
+        # Here we tests that weird values provided as parameter are rejected with an exception
         with self.assertRaises(RuntimeError):
             faller.start(faller.MotorCrab, "1")        
              
-    def test_stop(self):
-        faller.init("172.17.217.217")
-        for motor in [faller.MotorCrab, faller.MotorChassis, faller.MotorSpreader]:
-                faller.stop(motor)        
-
-    def test_stopInvalid(self):
-        faller.init("172.17.217.217")
-        with self.assertRaises(RuntimeError):
-            faller.stop(-5)        
-
-        with self.assertRaises(TypeError):
-            faller.stop("Yo") 
+   
 
     def test_get_battery(self):
         faller.init("172.17.217.217")
