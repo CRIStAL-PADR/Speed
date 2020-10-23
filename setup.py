c9390fc0910cd79eeff0ca6881ab35de3cc1223c
Damien Marchal <damien.marchal@univ-lille1.fr>
2020-10-23 15:12:25 +0200

    Add setup.py to be able to distribute the project.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="speedlib-fabrice-dedo",
+    version="1.0",
+    author="Fabrice Dedo & Damien Marchal",
+    author_email="komla-sam-fabrice.dedo@polytech-lille.net",
+    description="A python library to operate 'Speed' devices",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CRIStAL-PADR/Speed",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
