791230eae71a61afb1c7f1e4a983566a7b70dfbb
Fabrice dedo <dsamfabrice5@gmail.com>
2020-10-29 11:13:20 +0100

    Correction made to the file prehenseur.py

--- speedlib/cranes/prehenseur.py
+++ speedlib/cranes/prehenseur.py
@@ -1,11 +1,9 @@
 from pint import UnitRegistry
 
-def gripper(sNr, x = 1 , n=0 ):
+def gripper( n=0 ):
     global timeout
     """  It is a function to control the gripper
-        : param :  sNr This is the motor on which the magnet will be. It must be 2 because the magnet will be on the motorCrab
         : param : x is the parameter which allows to activate or not the magnet. 
-            We will take the values ​​1 to activate and 0 to switch off
          This sensor has as output d which is the distance which separates it from an obstacle (I take here the ceiling of the container)
         :param d :  it gives a certain value that we can read
         
@@ -13,18 +11,12 @@ def gripper(sNr, x = 1 , n=0 ):
     """
 
     d = sensor (n) * ureg.meter
-    x = 1 # initial state value (convention)
-    if sNr !=2 :
-        raise ValueError (" The expected value is 2 but  got ".format(sNr))
-    if x !=0 or x!=1 : 
-        raise ValueError ("The value must be  0 soit 1 but got ".format(x))
-    if not isinstance(x, int) :
-        raise TypeError("x must be integer type")
+    x = False # initial state value (convention)
     if d > 0.002 *ureg.meter : #for exemple
-        x = 0 # he magnet is disabled
+        x = False # The magnet is disabled
     if d < 0.002 * ureg.meter : # for exemple
-        x = 1 #he magnet is disabled
-    ip, numMotor = get_motor_info_from_number(sNr, n)
+        x = True # The magnet is disabled
+    ip, numMotor = get_motor_info_from_number(2, n)
     
     r = requests.get("http://"+ip+"/gripper?sNr="+str(numMotor)+"&x="+str(x),timeout=timeout)
     
