2fa9221233d28fdabe9531ae100d8c3d69c224ac
samFabrice <dsamfabrice5@gmail.com>
2021-02-12 12:37:56 +0100

    I put train1 on address 3 to check that the motor  is running and to control the lights

--- examples/train_example.py
+++ examples/train_example.py
@@ -2,19 +2,15 @@ from speedlib.trains import dcc
 from speedlib.trains.dcc import Train
 
 
-train1 = Train("DCC4",4) # Create the DCC controller with the RPi encoder, Create locos (args: Name, DCC Address) and Register locos on the controller
+train1 = Train("DCC3",3) # Create the DCC controller with the RPi encoder, Create locos (args: Name, DCC Address) and Register locos on the controller
 train2 = Train("DCC1",1)
 
 dcc.start() #Start the controller. Removes brake signal
 
 train1.l.speed = 14 # set de speed to 14
-
-train1.faster() # Increase speed by one step
-
 train1.slower() # Reduce speed by one step
 train1.reverse() #
 train1.l.fl = True # Change fl function bit and allows to light it
-train1.l.f2 = False
-train1.stop() # Stop the controller
-
+train1.l.f2 = True
+dcc.stop() # Stop the controller
 
