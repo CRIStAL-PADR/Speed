80f879053ba79130f1561cab623d66639542fc5e
samFabrice <dsamfabrice5@gmail.com>
2021-05-03 13:59:38 +0200

    added the class Switch to the train to control driving brains

--- speedlib/trains/dcc.py
+++ speedlib/trains/dcc.py
@@ -3,8 +3,10 @@
 from dccpi import *
 
 
+
 e = DCCRPiEncoder()
 controller = DCCController(e) #Create the DCC controller with the RPi encoder
+
 def start():
     """ Starts the controller and Removes a brake signal """
     controller.start()
@@ -25,7 +27,7 @@ class Train:
             raise TypeError(" name must be a str but got " +str(name))
         if not isinstance(adress, int):
             raise TypeError("adress must be an integer but got  " +str(adress))
-        if adress not in rangegit(2, 126):
+        if adress not in rangegit(2, 100):
             raise RuntimeError("""The address must be between 1 and 127 but preferably 3
              which is the factory address but got """+str(adress))
 
@@ -131,3 +133,24 @@ class Train:
             raise TypeError("var must be a bool but got "+str(var))
         self._l.f4 = var
     f4 = property(_get_f4, _set_f4)
+
+class Switch(Train):
+
+    def __init__(self, name, adress):
+            
+        if not isinstance(name, str):
+            raise TypeError(" name must be a str but got " +str(name))
+        if not isinstance(adress, int):
+            raise TypeError("adress must be an integer but got  " +str(adress))
+        if adress not in rangegit(101, 120):
+            raise RuntimeError("""The address must be between 1 and 127 but preferably 3
+             which is the factory address but got """+str(adress))
+             
+        Personne.__init__(self, name, adress)
+        self._l = DCCLocomotive(name, adress)  # Create locos, args: Name, DCC Address
+        controller.register(self._l)  # Register locos on the controller
+        self.fl = False
+        self.f1 = False
+        self.f2 = False
+        self.f3 = False
+        self.f4 = False
