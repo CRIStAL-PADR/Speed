4947f7538a296a4af85e5b01c63df1d0d809ee55
samFabrice <dsamfabrice5@gmail.com>
2021-05-03 21:23:28 +0200

    I used pylint to do some refactoring on the code

--- speedlib/dcc/dcc_trains.py
+++ speedlib/dcc/dcc_trains.py
@@ -1,7 +1,14 @@
-import dcc_object
-from dcc_object import DCCObject
+"""
+    Created on Tue May 4 2021
+    @author : fabrice
+"""
+# -*-coding: <Utf-8> -*-
+from Speed.speedlib.dcc.dcc_object import DCCObject
 
 class Train:
+    """
+        Train class
+    """
     def __init__(self, name, adress):
         """ this function takes a name and an address (an integer # 0) as
         parameters to create a train and  register it  on the controller
@@ -14,39 +21,40 @@ class Train:
             raise RuntimeError("""The address must be between 1 and 127 but
               got """+str(adress))
         self.dccobject = DCCObject(name, adress)
-    
+
     def faster(self):
+        """ Increase 1 speed step"""
         self.dccobject.faster()
-        
+
     def reverse(self):
         """Change the direction"""
         self.dccobject.reverse()
-    
+
     def slower(self):
         """Reduce the speed"""
         self.dccobject.slower()
-    
+
     def __repr__(self):
         return self.dccobject.__repr__()
-        
+
     def _get_speed(self):
         """ Returns the current speed of the train """
         return self.dccobject.speed
-    
+
     def _set_speed(self, new_speed):
         """ change the speed"""
         self.dccobject.speed = new_speed
     speed = property(_get_speed, _set_speed)
-        
-    def _get_fl(self):
-        """Returns the current state of fl """
-        return self.dccobject.fl
 
-    def _set_fl(self, var):
+    def _get_f_light(self):
+        """Returns the current state of fl """
+        return self.dccobject.f_light
+
+    def _set_f_light(self, var):
         """ change the state of fl """
-        self.dccobject.fl = var
-    fl = property(_get_fl, _set_fl)
-    
+        self.dccobject.f_light = var
+    f_light = property(_get_f_light, _set_f_light)
+
     def _get_f1(self):
         """Returns the current state of f1 """
         return self.dccobject.f1
@@ -55,7 +63,7 @@ class Train:
         """ change the state of f1 """
         self.dccobject.f1 = var
     f1 = property(_get_f1, _set_f1)
-    
+
     def _get_f2(self):
         """Returns the current state of f1 """
         return self.dccobject.f1
@@ -64,7 +72,7 @@ class Train:
         """ change the state of f1 """
         self.dccobject.f2 = var
     f2 = property(_get_f2, _set_f2)
-    
+
     def _get_f3(self):
         """Returns the current state of f1 """
         return self.dccobject.f3
@@ -73,7 +81,7 @@ class Train:
         """ change the state of f1 """
         self.dccobject.f3 = var
     f3 = property(_get_f3, _set_f3)
-    
+
     def _get_f4(self):
         """Returns the current state of f1 """
         return self.dccobject.f4
@@ -82,14 +90,9 @@ class Train:
         """ change the state of f1 """
         self.dccobject.f4 = var
     f4 = property(_get_f4, _set_f4)
-    
-    
-    
-    
 
 if __name__ == "__main__":
-    
-    train1 = Train("DCC3",99)
-    train1.speed = 15
-    train1.fl = True
-    print(train1)
+    train = Train("DCC3", 99)
+    train.speed = 15
+    train.f_light = True
+    print train
